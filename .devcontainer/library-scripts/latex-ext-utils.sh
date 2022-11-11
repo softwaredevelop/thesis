71248d57c7537bbbaf1be4cf046835d774715447
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-11-11 11:35:00 +0000

    refactor: Refactor processes

--- .devcontainer/library-scripts/latex-ext-utils.sh
+++ .devcontainer/library-scripts/latex-ext-utils.sh
@@ -3,20 +3,32 @@
 
 set -e
 
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  exit 1
+fi
+
+function apt_get_update_if_needed() {
+  if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" -eq 0 ]; then
+    apt-get update
+  fi
+}
+
+function check_packages() {
+  if ! dpkg --status "$@" >/dev/null 2>&1; then
+    apt_get_update_if_needed
+    apt-get install --no-install-recommends --assume-yes "$@"
+  fi
+}
+
 export DEBIAN_FRONTEND=noninteractive
 
-if [[ -d "/var/lib/apt/lists" && $(ls /var/lib/apt/lists/ | wc -l) -eq 0 ]]; then
-  if apt-get update; then
-    LISTS_UPDATED=0
-  fi
+if ! command -v cpan >/dev/null 2>&1 || ! command -v perl >/dev/null 2>&1 || ! command -v cpanm >/dev/null 2>&1; then
+  apt_get_update_if_needed
+  check_packages \
+    perl \
+    cpanminus
 fi
 
-if (("$LISTS_UPDATED" == 0)); then
-  package_list="gcc \
-        build-essential"
-  apt-get install --no-install-recommends --assume-yes $package_list
-  if command -v cpan >/dev/null 2>&1 && command -v perl >/dev/null 2>&1; then
-    /usr/bin/cpan Log::Dispatch::File YAML::Tiny
-    /usr/bin/perl -MCPAN -e 'install "File::HomeDir"'
-  fi
-fi
+cpanm YAML::Tiny File::HomeDir Log::Dispatch::File
+perl -MCPAN -e 'install "File::HomeDir"'
