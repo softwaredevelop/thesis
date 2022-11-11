734aea13f73de7cf7a8e6cb0b6e4161587fb6e73
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-11-11 07:45:54 +0000

    test: Add latex packages test

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
 SCRIPT_FOLDER_NAME=$(dirname $0)
 cd ${SCRIPT_FOLDER_NAME} || exit
@@ -35,5 +36,48 @@ check "shfmt" shfmt --version
 check "shellcheck" shellcheck --version
 check "editorconfig" ec --version
 check "trivy" trivy --version
+if [ "${ID_OS}" = "ubuntu" ]; then
+  package_list_latex="biber \
+        chktex \
+        cm-super \
+        dvidvi \
+        dvipng \
+        fragmaster \
+        git \
+        lacheck \
+        latexdiff \
+        latexmk \
+        lcdf-typetools \
+        lmodern \
+        make \
+        psutils \
+        purifyeps \
+        t1utils \
+        tex-gyre \
+        texinfo \
+        texlive-base \
+        texlive-bibtex-extra \
+        texlive-binaries \
+        texlive-extra-utils \
+        texlive-font-utils \
+        texlive-fonts-extra \
+        texlive-fonts-extra-links \
+        texlive-fonts-recommended \
+        texlive-formats-extra \
+        texlive-lang-english \
+        texlive-lang-european \
+        texlive-latex-base \
+        texlive-latex-extra \
+        texlive-latex-recommended \
+        texlive-luatex \
+        texlive-metapost \
+        texlive-pictures \
+        texlive-plain-generic \
+        texlive-pstricks \
+        texlive-science \
+        texlive-xetex"
+
+  checkOSPackages "latex" ${package_list_latex}
+fi
 
 reportResults
